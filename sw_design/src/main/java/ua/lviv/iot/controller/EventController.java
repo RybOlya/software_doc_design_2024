34d0c21acc9ebc2122b2d8936db90254a8964e9d
RybOlya <olga9rybenchuk@gmail.com>
2024-04-02 01:50:01 +0300

    Implementing controllers

--- sw_design/src/main/java/ua/lviv/iot/controller/EventController.java
+++ sw_design/src/main/java/ua/lviv/iot/controller/EventController.java
@@ -1,4 +1,53 @@
 package ua.lviv.iot.controller;
 
-public class EventController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.lviv.iot.model.Event;
+import ua.lviv.iot.model.Order;
+import ua.lviv.iot.service.EventService;
+import ua.lviv.iot.service.OrderService;
+
+@Controller
+public class EventController extends AbstractController<Event>{
+    private final EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        super(eventService);
+        this.eventService = eventService;
+    }
+
+    @GetMapping(path = "/event")
+    public String getAllEvents(Model model) {
+        model.addAttribute("event", eventService.findAll());
+        return "event";
+    }
+
+    @GetMapping(path = "/showNewEventForm")
+    public String showNewEventForm(Model model) {
+        Event event = new Event();
+        model.addAttribute("event", event);
+        return "create_event";
+    }
+
+    @GetMapping(path = "/showUpdateEventForm/{id}")
+    public String showUpdateEventForm(@PathVariable("id") Integer eventId, Model model) {
+        Event event = eventService.getById(eventId);
+        model.addAttribute("event", event);
+        return "update_event";
+    }
+
+    @PostMapping(path = "/saveEvent")
+    public String saveEvent(@ModelAttribute("event") Event event) {
+        eventService.saveToDatabase(event);
+        return "redirect:/event";
+    }
+
+    @GetMapping(path = "/deleteEvent/{id}")
+    public String deleteEvent(@PathVariable("id") Integer id) {
+        eventService.deleteById(id);
+        return "redirect:/event";
+    }
 }
--- sw_design/src/main/java/ua/lviv/iot/controller/TicketController.java
+++ sw_design/src/main/java/ua/lviv/iot/controller/TicketController.java
@@ -1,4 +1,47 @@
 package ua.lviv.iot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ua.lviv.iot.dto.TicketDto;
+import ua.lviv.iot.model.Ticket;
+import ua.lviv.iot.service.TicketService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/ticket")
+@RestController
 public class TicketController {
+
+    private final TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+    @GetMapping
+    public ResponseEntity<List<TicketDto>> getAllTickets() {
+        List<Ticket> tickets = ticketService.findAll();
+        List<TicketDto> ticketDtos = new ArrayList<>();
+        for (Ticket ticket : tickets) {
+            TicketDto ticketDto = new TicketDto(ticket.getId(), ticket.getEvent().getId(),
+                    ticket.getTicketType(), ticket.getPrice());
+            ticketDtos.add(ticketDto);
+        }
+        return new ResponseEntity<>(ticketDtos, HttpStatus.OK);
+    }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<TicketDto> getTicket(@PathVariable("id") Integer ticketId) {
+        Ticket updatedTicket = ticketService.getById(ticketId);
+        if (updatedTicket!=null) {
+            TicketDto ticketDto = new TicketDto(updatedTicket.getId(), updatedTicket.getEvent().getId(),
+                    updatedTicket.getTicketType(), updatedTicket.getPrice());
+            return new ResponseEntity<>(ticketDto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
